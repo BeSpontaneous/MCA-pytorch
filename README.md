08920779a9316fd1a2476c9e47559801b495e07f
BeSpontaneous <41779062+BeSpontaneous@users.noreply.github.com>
2022-11-02 11:06:40 -0400

    submit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# GhostMotion
+# Probing into Overfitting for Video Recognition
+
+## Reuiqrements
+- python 3.7
+- pytorch 1.9.0
+- torchvision 0.10.0
+
+## Prepare datasets
+Put the directory of datasets in `ROOT_DATASET` under `ops/dataset_config.py`
+
+## Training TSM on Something-Something V1 using standard protocal
+```
+bash sth.sh
+```
+
+## Training TSM on Something-Something V1 with Ghost Motion and Logits Smoothing
+```
+bash sth_GM.sh
+```
---
+++ sth_GM.sh
@@ -0,0 +1,10 @@
+###### training with Ghost Motion
+
+
+CUDA_VISIBLE_DEVICES=0,1 python main.py something RGB \
+     --arch_file resnet_TSM \
+     --arch resnet50 --num_segments 8 \
+     --amp --gd 20 --lr 0.02 --lr_steps 20 40 --epochs 50 \
+     --batch-size 64 -j 12 --dropout 0.5 --consensus_type=avg --eval-freq=1 \
+     --temperature 3 --beta 1 --GM_prob 0.5 --model_path 'models' \
+     --shift --shift_div=8 --shift_place=blockres --npb --round 1;
